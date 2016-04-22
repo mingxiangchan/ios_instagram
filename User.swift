3e98101fba1b6c2244accf1fc91d74be0bb3617b
Abdo Assem <aassem@abdos-macbook-pro.local>
2016-04-22 14:12:13 +0800

    Added User Class (username)

---
+++ User.swift
@@ -0,0 +1,24 @@
+//
+//  User.swift
+//  
+//
+//  Created by Abdo Assem on 4/22/16.
+//
+//
+
+import Foundation
+class User{
+    private let _userkey: String!
+    var username: String!
+    
+    var userkey:String!{
+        return _userkey
+    }
+    
+    init (key: String, dict: [String : AnyObject]){
+        self.-_userkey=key
+        if let username = dict ["username"] as? String{
+            self.usrname=username
+        }
+}
+}
