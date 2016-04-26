61724cd384ed3346c206be2ef43cd3c4e06b0f90
mxchan <mingxiangchan@gmail.com>
2016-04-26 14:13:38 +0800

    added firebase data persistence

---
+++ ios_instagram/CommentsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CommentsViewController.swift
+//  ios_instagram
+//
+//  Created by Ming Xiang Chan on 26/04/2016.
+//  Copyright © 2016 NEXTACADEMY. All rights reserved.
+//
+
+import UIKit
+
+class CommentsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
